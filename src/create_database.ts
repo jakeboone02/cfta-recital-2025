972fff749d069325bdc6506218f5fae2ccdd3b1b
Jake Boone <jakeboone02@gmail.com>
2025-01-25 00:27:25 -0700

    Get started on 2025 recital

---
+++ src/create_database.ts
@@ -0,0 +1,44 @@
+import { Database } from 'bun:sqlite';
+
+const db = new Database();
+
+const statements = (await Bun.file('./create_database.sql').text())
+  .split(/;[\r\n]+/g)
+  .filter(s => s.startsWith('CREATE') || s.startsWith('INSERT'));
+
+for (const sql of statements) {
+  // console.log(sql);
+  db.run(sql);
+}
+
+// console.log(db.query('SELECT count(*) dance_count FROM dances').all()[0]);
+// console.log(
+//   db.query('SELECT count(*) spectapular_count FROM dances where spectapular = 1').all()[0]
+// );
+// console.log(db.query('SELECT count(*) dancer_count FROM dancers').all()[0]);
+// console.log(db.query('SELECT count(*) dance_dancer_count FROM dance_dancers').all()[0]);
+
+console.log(
+  db
+    .query(
+      `
+  SELECT recital_group, dancer, group_concat(dance, '|') dance_list, count(*) dance_dancer_count
+    FROM dances INNER JOIN dance_dancers ON dances.id = dance_id
+   WHERE recital_group IN (1, 2, 3)
+     AND dance NOT LIKE '%TAP'
+   GROUP BY recital_group, dancer
+  HAVING dance_dancer_count > 1
+  ORDER BY dance_dancer_count DESC, recital_group ASC, dancer ASC`
+    )
+    .all()
+    .map(
+      r =>
+        // @ts-ignore
+        `${r.dance_dancer_count} dances in group ${r.recital_group}:  ${r.dancer}  (${r.dance_list
+          .split('|')
+          // @ts-ignore
+          .map(d => `[${d}]`)
+          .join(', ')})`
+    )
+    .join('\n')
+);
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/cfta-recital-2024/',
+  base: '/cfta-recital-2025/',
   plugins: [react()],
 });
